e586cd3f3382c0efb16ffc31bb1bd7d00ef2c37a
Colin McNish <54324572+danfow@users.noreply.github.com>
2021-10-07 12:10:52 -0500

    Add files via upload

--- ParserProjectCS441/ParserProject.rkt
+++ ParserProjectCS441/ParserProject.rkt
@@ -53,18 +53,35 @@
       [ ( eq? (char-alphabetic? char)   #t)]
       [else #f]))
 
+(define (isDigit? char)
+(cond 
+      [ (or ( eq? (char-numeric? char)   #t) (eq? #\. char))]
+      [else #f]))
+
+(define (isAssign? char)
+(cond 
+      [ ( equal? char #\=) #t ]
+      [else #f]))
+
 (define (toId charLists tokenList wordString)
 (cond 
       [(and  (not(isLetter? (first charLists))) (and (not ( string=? wordString "read")) (not (string=? wordString "write")))) (cons "ID" charLists)]
       [(and (equal? wordString "write")) (not(isLetter? (first charLists))) (cons "write" charLists)]
        [(and (equal? wordString "read")) (not(isLetter? (first charLists))) (cons "read" charLists)]
       [(isLetter? (first charLists)) (toId (rest charLists) tokenList (string-append wordString (list->string  (list (first charLists)))))]
-      [else "error"] ; need more cases to output if its a read or write or not but brain hurt
+      [else "error"] 
+))
+
+(define (toNum charLists tokenList wordString)
+(cond 
+      [(not(isDigit? (first charLists))) (cons "Num" charLists)]
+      [(isDigit? (first charLists)) (toNum (rest charLists) tokenList (string-append wordString (list->string  (list (first charLists)))))]
+      [else "error"] 
 ))
 
 
 (define (scanner charList tokenList )
-  (cond [ (or (null? charList) (and(equal? #\$ (first charList)) (equal? #\$ (first(rest charList))))) tokenList]
+  (cond [ (or  (and(equal? #\$ (first charList)) (equal? #\$ (first(rest charList)))) (null? charList)) tokenList]
         [ (equal? #t (isMultOpToken? (first charList) ))  (scanner(rest charList) (append tokenList (list "MultOp") ))] 
         [ (equal? #t (isAddOpToken? (first charList) ) )  (scanner(rest charList) (append tokenList (list "addOp") ))]
         [ (equal? #t (isLeftParen? (first charList) ) )  (scanner(rest charList) (append tokenList (list "lParen") ))]
@@ -72,14 +89,17 @@
         [ (and (equal? "ID" (first(toId charList tokenList ""))) (equal? #t (isLetter? (first charList)))) (scanner (rest(toId charList tokenList "")) (append tokenList (list "ID")))]
         [ (and (equal? "read" (first(toId charList tokenList ""))) (equal? #t (isLetter? (first  charList)))) (scanner (rest(toId charList tokenList "")) (append tokenList (list "read")))]
         [ (and (equal? "write" (first(toId charList tokenList ""))) (equal? #t (isLetter? (first charList)))) (scanner (rest(toId charList tokenList "")) (append tokenList (list "write")))]
-        
-        
-        
-        [else (scanner(rest charList) tokenList)]
+        [ (and (equal? "Num" (first(toNum charList tokenList ""))) (equal? #t (isDigit? (first charList)))) (scanner (rest(toNum charList tokenList "")) (append tokenList (list "Num")))]
+        [ (equal? #t (char-whitespace?  (first charList))) (scanner (rest charList) tokenList)]
+        [ (and  (equal? #\:  (first charList))) (equal? #\=  (first (rest charList)))   (scanner (rest charList) (append tokenList (list "assignment")))] ;don't understand why this does not work
+        [else "error" ] ;(scanner(rest charList) tokenList)
 ))
 (define (file1) (fileToCharString "Input01.txt"))
+(define (file2) (fileToCharString "Input02.txt"))
 (define (file3) (fileToCharString "Input03.txt"))
 (define (file4) (fileToCharString "Input04.txt"))
+(define (file5) (fileToCharString "Input05.txt"))
+(define (file6) (fileToCharString "Input06.txt"))
 
 
 
