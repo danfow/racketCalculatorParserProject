150999aa68ceeb2249a8b55325168b145fd13f7e
Colin McNish <54324572+danfow@users.noreply.github.com>
2021-10-06 22:49:27 -0500

    Add files via upload

--- ParserProjectCS441/ParserProject.rkt
+++ ParserProjectCS441/ParserProject.rkt
@@ -48,13 +48,33 @@
       [else #f]))
 
 
+(define (isLetter? char)
+(cond 
+      [ ( eq? (char-alphabetic? char)   #t)]
+      [else #f]))
 
-(define (scanner charList tokenList)
+(define (toId charLists tokenList wordString)
+(cond 
+      [(and  (not(isLetter? (first charLists))) (and (not ( string=? wordString "read")) (not (string=? wordString "write")))) (cons "ID" charLists)]
+      [(and (equal? wordString "write")) (not(isLetter? (first charLists))) (cons "write" charLists)]
+       [(and (equal? wordString "read")) (not(isLetter? (first charLists))) (cons "read" charLists)]
+      [(isLetter? (first charLists)) (toId (rest charLists) tokenList (string-append wordString (list->string  (list (first charLists)))))]
+      [else "error"] ; need more cases to output if its a read or write or not but brain hurt
+))
+
+
+(define (scanner charList tokenList )
   (cond [ (or (null? charList) (and(equal? #\$ (first charList)) (equal? #\$ (first(rest charList))))) tokenList]
         [ (equal? #t (isMultOpToken? (first charList) ))  (scanner(rest charList) (append tokenList (list "MultOp") ))] 
         [ (equal? #t (isAddOpToken? (first charList) ) )  (scanner(rest charList) (append tokenList (list "addOp") ))]
         [ (equal? #t (isLeftParen? (first charList) ) )  (scanner(rest charList) (append tokenList (list "lParen") ))]
         [ (equal? #t (isRightParen? (first charList) ) )  (scanner(rest charList) (append tokenList (list "RParen") ))]
+        [ (and (equal? "ID" (first(toId charList tokenList ""))) (equal? #t (isLetter? (first charList)))) (scanner (rest(toId charList tokenList "")) (append tokenList (list "ID")))]
+        [ (and (equal? "read" (first(toId charList tokenList ""))) (equal? #t (isLetter? (first  charList)))) (scanner (rest(toId charList tokenList "")) (append tokenList (list "read")))]
+        [ (and (equal? "write" (first(toId charList tokenList ""))) (equal? #t (isLetter? (first charList)))) (scanner (rest(toId charList tokenList "")) (append tokenList (list "write")))]
+        
+        
+        
         [else (scanner(rest charList) tokenList)]
 ))
 (define (file1) (fileToCharString "Input01.txt"))
